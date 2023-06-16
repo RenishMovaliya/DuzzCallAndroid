10f0e5c20b18c6905b543240ef4044731537637a
RenishMovaliya <movaliyarenish@gmail.com>
2023-06-16 08:58:20 +0530

    -add design

--- app/src/main/java/com/logycraft/duzzcalll/DashboardActivity.kt
+++ app/src/main/java/com/logycraft/duzzcalll/DashboardActivity.kt
@@ -1,11 +1,52 @@
 package com.logycraft.duzzcalll
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.logycraft.duzzcalll.fragment.DialFragment
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+//        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.favourite -> {
+//                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.history -> {
+//                    loadFragment(ChatFragment())
+                    true
+                }
+                R.id.contact -> {
+//                    loadFragment(SettingFragment())
+                    true
+                }
+                R.id.dialpad -> {
+                    loadFragment(DialFragment())
+                    true
+                }
+                R.id.setting -> {
+//                    loadFragment(SettingFragment())
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
